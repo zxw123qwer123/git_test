6b49c3a59a3f54d7285cf6ebb0a4dbb427c9de3f
zhangxiaowen <18355519361@163.com>
2019-07-11 11:41:34 +0800

    解决

---
+++ git_test_project/src/com/hf/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.hf.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+    }
+}
